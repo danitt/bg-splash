28dc4d3dbdcbf5551bb8157f7624abe67da6fcb9
dan <dan@danitt.com>
2019-03-28 16:49:02 +1000

    fix cron shell

--- bg-splash.sh
+++ bg-splash.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-./node_modules/.bin/ts-node index.ts
+# Add node to path
+export PATH="/usr/local/bin:$PATH"
+# Add yarn to path
+export PATH="$HOME/.yarn/bin:$PATH"
+
+# Compose path to project
+REL_PATH="`dirname \"$0\"`"              # relative pwd
+ABS_PATH="`( cd \"$REL_PATH\" && pwd )`"  # absolutized and normalized path
+
+# Launch script
+yarn --cwd $ABS_PATH start
