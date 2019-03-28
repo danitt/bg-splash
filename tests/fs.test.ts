ba439462fbfe1e3179a59996541ccdd09de07651
danitt <dan@danitt.com>
2019-03-28 17:02:58 +1000

    Update fs.test.ts

--- tests/fs.test.ts
+++ tests/fs.test.ts
@@ -4,13 +4,11 @@ describe('FS tests', () => {
 
   test('Should store buffer locally', async (done) => {
     const newBuffer = Buffer.alloc(2, 0);
-    let hasError = false;
     try {
       await saveBufferToLocal(newBuffer, 'buffertest.jpg');
     } catch (e) {
-      hasError = true;
+      expect(hasError).toBeFalsy();
     }
-    expect(hasError).toBeFalsy();
     done();
   });
 
