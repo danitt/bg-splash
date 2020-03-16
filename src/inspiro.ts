573a32c024b7f128515b5e2f8cca36486926b163
Dan <dan@danitt.com>
2020-03-16 15:31:43 +1000

    inspiro api logic

---
+++ src/inspiro.ts
@@ -0,0 +1,21 @@
+// Dependencies
+import nodeFetch from 'node-fetch';
+
+// Typings
+import { ImageObject } from '@src/typings';
+
+// Constants
+const INSPIRO_URL = 'https://inspirobot.me/api?generate=true';
+
+export async function fetchRandomImage(): Promise<ImageObject> {
+  const imageSrc = await (await nodeFetch(INSPIRO_URL)).text();
+  const imageBuffer = await (await nodeFetch(imageSrc)).buffer();
+  // Parse filename
+  const regex = new RegExp(/\/a\/(.*)\.jpg/g).exec(imageSrc); //?
+  const filename = regex.length > 1 ? regex[1] : String(new Date().getTime());
+
+  return {
+    filename,
+    buffer: imageBuffer,
+  };
+}
