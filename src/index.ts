9ec12edd4c1d147770b6caba4e2c5065f61fa082
Dan <dan@danitt.com>
2020-03-16 15:37:02 +1000

    add cli flag to swap image sources

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
-import { fetchRandomImage } from '@src/unsplash';
+import * as minimist from 'minimist';
+
+import { fetchRandomImage as fetchUnsplash } from '@src/unsplash';
+import { fetchRandomImage as fetchInspiro } from '@src/inspiro';
 import { updateBackground } from '@src/bg';
 import { saveBufferToLocal } from '@src/fs';
+
+// Parse CLI Flags
+const args = minimist(process.argv.slice(2));
+
+// Determine image source
+const sourceFlag = Object.entries(args).find(([key]) => key === 'source')
+const fetchFn = sourceFlag && sourceFlag[1] === 'inspiro' ? fetchInspiro : fetchUnsplash;
 
 // IIFE to launch script, and enable async/await
 (async () => {
   // Get random image
-  const randomImage = await fetchRandomImage();
+  const randomImage = await fetchFn();
   const { filename, buffer } = randomImage;
   // Save to local directory
   await saveBufferToLocal(buffer, filename);
